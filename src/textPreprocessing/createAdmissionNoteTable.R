f6652c22439a2ae8629ad066a526cbcd71cbe312
sandeepayyar <sandeepayyar19@gmail.com>
2017-03-04 19:32:49 -0800

    Update createAdmissionNoteTable.R

--- src/textPreprocessing/createAdmissionNoteTable.R
+++ src/textPreprocessing/createAdmissionNoteTable.R
@@ -68,7 +68,99 @@ icd9NotesDataTable = right_join(x = diagnoses, y = notes, by = 'HADM_ID')
 # filter out any words not needed
 # filter out and icd9 codes not needed
 
-
+#####################################################################################
+# Add two additional columns
+# 1. Level 2 Categories: Process all ICD-9 codes and convert them to the 3 digit disease categories
+# 2. ICD-9 Top Level Categories: process all ICD-9 codes and convert them to their parent categories (19)
+# https://en.wikipedia.org/wiki/List_of_ICD-9_codes
+#####################################################################################
+for (i in 1:nrow(icd9NotesDataTable)){
+  print(i)
+  icd9row = str_split(icd9NotesDataTable$ICD9_CODE[i], '-')[[1]]
+  #icd9List = (sapply(icd9row, function(x) substr(x,1, 3),USE.NAMES = F))
+  #icd9NotesDataTable[i,9] = paste(icd9List, collapse = '-')
+  icd9ListLevel2 = c()
+  icd9ListTop = c()
+  for (icd9 in icd9row){
+    icd9row = icd9row[icd9row!= ""]
+    if (length(icd9row) == 0) {
+      next
+    }
+    else
+    {
+      if (substr(icd9, 1, 1) == "E"){
+        icd9Level2 = substr(icd9, 1, 4)
+        icd9Top = "cat:19"
+      }
+      else if (substr(icd9, 1,1) == "V"){
+        icd9Level2 = substr(icd9, 1, 3)
+        icd9Top = "cat:19"
+      }
+      else {
+        icd9 = as.numeric(substr(icd9, 1, 3))
+        if (icd9 >= 001 && icd9 <= 139) {
+          icd9Top = "cat:1"
+        }
+        else if (icd9 >= 140 && icd9 <= 239){
+          icd9Top = "cat:2"
+        }
+        else if(icd9 >= 240 && icd9 <= 279){
+          icd9Top = "cat:3"
+        }
+        else if(icd9 >= 280 && icd9 <= 289){
+          icd9Top = "cat:4"
+        }
+        else if (icd9 >= 290 && icd9 <= 319){
+          icd9Top = "cat:5"
+        }
+        else if(icd9 >= 320 && icd9 <= 359){
+          icd9Top = "cat:6"
+        }
+        else if(icd9 >= 360 && icd9 <= 389){
+          icd9Top = "cat:7"
+        }
+        else if (icd9 >= 390 && icd9 <= 459){
+          icd9Top = "cat:8"
+        }
+        else if (icd9 >= 460 && icd9 <= 519){
+          icd9Top = "cat:9"
+        }
+        else if (icd9 >= 520 && icd9 <= 579){
+          icd9Top = "cat:10"
+        }
+        else if (icd9 >= 580 && icd9 <= 629){
+          icd9Top = "cat:11"
+        }
+        else if (icd9 >= 630 && icd9 <= 679){
+          icd9Top = "cat:12"
+        }
+        else if (icd9 >= 680 && icd9 <= 709){
+          icd9Top = "cat:13"
+        }
+        else if (icd9 >= 710 && icd9 <= 739){
+          icd9Top = "cat:14"
+        }
+        else if (icd9 >= 740 && icd9 <= 759){
+          icd9Top = "cat:15"
+        }
+        else if (icd9 >= 760 && icd9 <= 779){
+          icd9Top = "cat:16"
+        }
+        else if (icd9 >= 780 && icd9 <= 799){
+          icd9Top = "cat:17"
+        }
+        else if (icd9 >= 800 && icd9 <= 999){
+          icd9Top = "cat:18"
+        }
+      }
+    }
+    icd9ListTop = c(icd9ListTop, icd9Top)
+    icd9ListLevel2 = c(icd9ListLevel2, icd9)
+    }
+  icd9NotesDataTable[i,9] = paste(icd9ListLevel2, collapse = '-')
+  icd9NotesDataTable[i,10] = paste(icd9ListTop, collapse = '-')
+  #break;
+}
 
 
 ##################################################################################
