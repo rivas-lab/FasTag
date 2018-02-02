1bdeb601bb4281a02ac30c3763968911735c067f
ashleyz413 <ashleyz413@yahoo.com>
2018-02-02 15:21:33 -0800

    Add files via upload

---
+++ train_test_split.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+#put absolute or relative path to csv with all data here
+df = pd.read_csv('final_csu_file', sep='\t')
+
+#set the proportion of train data here
+train_proportion = 0.7
+
+msk = np.random.rand(len(df)) <= train_proportion
+
+train = df[msk]
+test = df[~msk]
+
+train.to_csv('csu_snomed_train', sep='\t', index=False)
+test.to_csv('csu_snomed_test', sep='\t', index=False)
